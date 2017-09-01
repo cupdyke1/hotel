908ea4d97e7a186d1c3702d7ef8a40c0ac7a96b0
Cody Updyke <cupdyke@dfwwin5l5y262.paloaltonetworks.local>
2017-08-31 19:31:44 -0700

    Initial Commit

---
+++ hotel/hotel.h
@@ -0,0 +1,19 @@
+//
+//  hotel.h
+//  hotel
+//
+//  Created by Cody Updyke on 8/31/17.
+//  Copyright © 2017 support. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hotel.
+FOUNDATION_EXPORT double hotelVersionNumber;
+
+//! Project version string for hotel.
+FOUNDATION_EXPORT const unsigned char hotelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hotel/PublicHeader.h>
+
+
